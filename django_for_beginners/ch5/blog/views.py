eab47cf6ca0a6ac22abe4cfc7b1a9187d4003d79
owari-taro <taro.biwajima@gmail.com>
2021-03-10 18:10:24 +0900

    add unit test case

--- django_for_beginners/ch5/blog/views.py
+++ django_for_beginners/ch5/blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, 
-    CreateView, UpdateView,DeleteView
-    
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from .models import Post
 from django.urls import reverse_lazy
 # Create your views here.
@@ -27,7 +26,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
 class BlogDeleteView(DeleteView):
-    model=Post
-    template_name='post_delete.html']
-    sucess_url=reverse_lazy("home")
+    model = Post
+    template_name = 'post_delete.html'
+    sucess_url = reverse_lazy("home")
