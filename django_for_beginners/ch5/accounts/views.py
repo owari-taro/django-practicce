ebd17c083d68bb645bd42b818c8966abc2d7326d
owari-taro <taro.biwajima@gmail.com>
2021-03-12 18:13:10 +0900

    fix a typo at accounts view

--- django_for_beginners/ch5/accounts/views.py
+++ django_for_beginners/ch5/accounts/views.py
@@ -6,6 +6,6 @@ from django.views import generic
 
 
 class SignUpView(generic.CreateView):
-    from_class = UserCreationForm
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
