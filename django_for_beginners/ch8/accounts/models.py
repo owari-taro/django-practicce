33f8b647509926d247f0771a4687d26d28a9870a
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-08 23:14:10 +0900

    rm ch8

--- django_for_beginners/ch8/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbsractUser
-
-
-
-
-# Create your models here.
-class CustomUser(AbstractUser):
-    age= models.PositiveIntegerFiled(null=True,blan=True)
