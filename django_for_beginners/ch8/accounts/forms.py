61058c25ef87f7124dc915f48fc4ea7a87390bd7
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-05-12 00:26:51 +0900

    update

---
+++ django_for_beginners/ch8/accounts/forms.py
@@ -0,0 +1,10 @@
+from djang import forms
+
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model=CustomUser
+        fields=UserCreationForm.Meta.Fields+("age",)
