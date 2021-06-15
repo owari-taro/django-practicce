622a64631246cf46fbb98a61fa08664aec6f43a0
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-06-15 19:39:40 +0900

    update

--- django_for_professionals/books/accounts/admin.py
+++ django_for_professionals/books/accounts/admin.py
@@ -1,3 +1,17 @@
+from django_for_professionals.books.accounts.forms import CustomUserChangeForm, CustomuserCreationForm
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
+CustomUser = get_user_model()
 # Register your models here.
+
+
+class CustomuserAdmin(UserAdmin):
+    add_form = CustomuserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', ]
+
+
+admin.site.register(CustomUser, CustomuserAdmin)
