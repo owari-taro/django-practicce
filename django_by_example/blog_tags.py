2e3f18d586a663a784e54354bd7d43b106838b23
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-06-25 14:31:13 +0900

    Update blog_tags.py

--- django_by_example/blog_tags.py
+++ django_by_example/blog_tags.py
@@ -11,3 +11,7 @@ def total_posts()
 def show_latest_posts(count=5):
   latest_posts=Post.published.order_by('-publish')[:count]
   return  {'latest_posts':latest_posts}
+
+@register.simple_tag
+def get_most_coommented_post(counts=5)
+  return Post.published.annotate(total_comments=Count('comments').order_by('-total_comments'))[]count
