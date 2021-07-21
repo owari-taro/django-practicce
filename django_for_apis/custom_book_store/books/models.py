ea37315ffc6f80ae34b832a535c87c295fb378d4
owari-taro <58625526+owari-taro@users.noreply.github.com>
2021-07-21 11:51:06 +0900

    add award model

--- django_for_apis/custom_book_store/books/models.py
+++ django_for_apis/custom_book_store/books/models.py
@@ -25,45 +25,46 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     is_special = models.BooleanField(default=False)
-   # comments = models.ForeignKey(
-    #    Comment, on_delete=models.CASCADE, related_name="book")
- #   publisher = models.ForeignKey(
-  #      Publisher,  related_name="books", on_delete=models.CASCADE)
-
-    # def save(self, force_insert: bool = False, force_update: bool = False,
-    #        using: Optional[str] = False, update_fields: Optional[Iterable[str]] = None):
-    #  print("save method")
-
-    # if self.comments.count() > CommentLimit:
-    #    raise TooManyException("")
-    # print(type(self.comments))
-    # print(self.comments.count())
-
-   #     super(Book, self).save()
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["author", "title"],
+                name="author_title_unique"
+            ),
+        ]
+
 
 class Comment(models.Model):
     comment = models.CharField(max_length=200)
-    # allow one to many
-    book = models.ForeignKey(
-        Book, on_delete=models.CASCADE, related_name="comments")
+    # allow one to man
+    book = models.ForeignKey(Book, null=True, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
         get_user_model(),  on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(null=True, auto_now_add=True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
 
     def __str__(self):
         return f"{self.comment}.{self.author}"
 
     def save(self, force_insert: bool = False, force_update: bool = False,
              using: Optional[str] = False, update_fields: Optional[Iterable[str]] = None):
-        print("save method")
-        if len(self.comment) ==0:
+       # print("save method")
+        if len(self.comment) == 0:
             raise Exception("empty string isnt allowed")
         if self.book.comments.count() > CommentLimit:
             raise TooManyRelationException("")
 
         super(Comment, self).save()
+
+
+class Award(models.Model):
+    # example model for testing manytomany field
+    name = models.CharField(max_length=250)
+    books = models.ManyToManyField(Book, null=True)
+    created_at = models.DateTimeField(null=True, auto_now_add == True)
+    updated_at = models.DateTimeField(null=True, auto_now=True)
