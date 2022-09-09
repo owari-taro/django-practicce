51d4376e4226f320831518f572d554abfa7df818
owari-taro <taro.biwajima@gmail.com>
2022-09-09 23:40:26 +0900

    lock

--- django_for_professionals/books/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.urls.resolvers import URLPattern
-from .views import BookListView, BookDetailView
-
-urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),
-    path('<uuid:pk>/', BookDetailView.as_view(), name="book_detail")
-
-]
