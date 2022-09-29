a8d2815b6568005e5b6a644c3fc01dad1593e8a0
owari-taro <taro.biwajima@gmail.com>
2022-09-29 11:17:50 +0900

    update pytest init

--- django_for_professionals/ch3/pages/tests.py
+++ django_for_professionals/ch3/pages/tests.py
@@ -3,13 +3,21 @@ import pytest
 
 # Create your tests here.
 from django.urls import reverse
+from pytest_django.asserts import assertTemplateUsed
 
 
-def test_homepage_status_code(client):
-    res = client.get("/")
-    assert res.status_code == 200
+@pytest.fixture(scope="function")
+def response(client):
+    # clientはfunction scope
+    return client.get(reverse("home"))
 
 
-def test_homepage_url_name(client):
-    res = client.get(reverse("home"))
-    assert res.status_code == 200
+def test_homepage_status_code(response):
+    # res = client.get("/")
+    print(response)
+    assert response.status_code == 200
+
+
+def test_homepage_url_name(response):
+    # res = client.get(reverse("home"))
+    assert response.status_code == 200
